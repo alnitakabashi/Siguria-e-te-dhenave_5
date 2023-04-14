f311a9930048c5fb121b146ce9fc9c9a6d4c8aac
Anila Luta <118193110+AnilaLuta@users.noreply.github.com>
2023-04-15 00:09:57 +0200

    Update Enkriptimi.java

--- Enkriptimi.java
+++ Enkriptimi.java
@@ -1,1 +1,20 @@
-
+public class Enkriptimi {
+  private char[] matrica = new char[5][5];
+  
+  public Enkriptimi(){
+  }
+  int row = 0, col = 0;
+  for(int i =0; i<key.length();i++){
+    char c = key.charAt(i);
+    if(c = 'J'){
+      c = 'I';
+  }
+  if(!contains(c)){
+    matrica[row][col] = c;
+    col++;
+    if(col == 5){
+    row++;
+    col=0;
+    }
+  }
+}
