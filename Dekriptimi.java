ba936046f2493914179997afabe37cf0ccdcf182
Anjeza Gashi <108656875+anjezagashi@users.noreply.github.com>
2023-04-15 00:14:06 +0200

    Update Dekriptimi.java
    Decrypt function

--- Dekriptimi.java
+++ Dekriptimi.java
@@ -31,4 +31,25 @@ public class Dekriptimi {
         }
         return false;
     }
+        
+        public String decrypt(String ciphertext) {
+        //Hiqni çdo karakter jo shkronje nga çelësi
+        ciphertext = ciphertext.replaceAll("[^a-zA-Z]", "").toUpperCase();
+
+        StringBuilder plaintext = new StringBuilder();
+        for (int i = 0; i < ciphertext.length(); i += 2) {
+            char a = ciphertext.charAt(i);
+            char b = ciphertext.charAt(i + 1);
+            int row1 = -1, col1 = -1, row2 = -1, col2 = -1;
+            for (int row = 0; row < 5; row++) {
+                for (int col = 0; col < 5; col++) {
+                    if (matrica[row][col] == a) {
+                        row1 = row;
+                        col1 = col;
+                    } else if (matrica[row][col] == b) {
+                        row2 = row;
+                        col2 = col;
+                    }
+                }
+            }
 
