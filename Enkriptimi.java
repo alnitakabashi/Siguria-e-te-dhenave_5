f20a5533cfd7a30193fa097c1ccc88907fcd2844
Anila Luta <118193110+AnilaLuta@users.noreply.github.com>
2023-04-15 15:07:34 +0200

    Create Enkriptimi.java

--- Enkriptimi.java
+++ Enkriptimi.java
@@ -19,28 +19,13 @@ public class Enkriptimi {
     }
   }
 }
-  public String encrypt(String plaintext) {
-        // Hiqni çdo karakter pa shkronjë nga teksti i thjeshtë dhe kthejeni në shkronja të mëdha
-        plaintext = plaintext.replaceAll("[^a-zA-Z]", "").toUpperCase();
-
-        // Shto një X midis shkronjave të njëpasnjëshme identike nëse është e nevojshme
-        StringBuilder sb = new StringBuilder();
-        sb.append(plaintext.charAt(0));
-        for (int i = 1; i < plaintext.length(); i++) {
-            char prev = sb.charAt(sb.length() - 1);
-            char curr = plaintext.charAt(i);
-            if (prev == curr) {
-                sb.append('X');
-            }
-            sb.append(curr);
+private boolean contains(char c){
+  for(int row = 0; row <5; row++){
+  for (int col =0; col < 5; col++){
+  if(matrica[row][col]==c}){
+    return true;
         }
-        if (sb.length() % 2 != 0) {
-            sb.append('X');
-        }
-     Enkriptoni çiftet e shkronjave duke përdorur matricen
-        StringBuilder ciphertext = new StringBuilder();
-        for (int i = 0; i < sb.length(); i += 2) {
-            char a = sb.charAt(i);
-            char b = sb.charAt(i + 1);
-
-
+      }
+    }
+  return false;
+}
