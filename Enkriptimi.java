3acff4b7c292b1e6b7b9a4d3eeb7abc35b1ca523
alnitakabashi <118002760+alnitakabashi@users.noreply.github.com>
2023-04-16 01:23:36 +0200

    Update Enkriptimi.java

--- Enkriptimi.java
+++ Enkriptimi.java
@@ -95,4 +95,12 @@ public static void main(String[]args){
   
   System.out.println("Ciphertext: " + ciphertext);
 }
+public void displaymatrica() {
+		// TODO Auto-generated method stub
+		for (int row = 0; row < 5; row++) {
+            for (int col = 0; col<5;col++) {
+                    System.out.print(matrica[row][col] + " ");
+                    }
+                    System.out.println();
+                }
 
