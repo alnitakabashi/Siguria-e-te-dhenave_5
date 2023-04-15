34d24720ca5d3e3b65ae0fcc72e4a24c94373077
AnjezaSfishta <115582023+AnjezaSfishta@users.noreply.github.com>
2023-04-15 22:59:24 +0200

    Update Dekriptimi.java

--- Dekriptimi.java
+++ Dekriptimi.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Dekriptimi {
     private char[][] matrica = new char[5][5];
 
@@ -33,6 +36,22 @@ public class Dekriptimi {
                     row++;
                     col = 0;
                 }
+                
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the key: ");
+                String key = scanner.nextLine();
+                Dekriptimi cipher = new Dekriptimi (key);
+
+                System.out.println("\nKey Square:");
+                cipher.displaymatrica();
+                System.out.print("\nEnter the ciphertext: ");
+                String ciphertext = scanner.nextLine();
+                String plaintext = cipher.decrypt(ciphertext);
+                System.out.println("Plaintext: " + plaintext);
+            }
+}
+
             }
         }
     }
