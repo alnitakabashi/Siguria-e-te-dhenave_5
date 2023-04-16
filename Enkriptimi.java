b034b3fb2005822ece8ab82adb74b11a736f2cf1
Anila Luta <118193110+AnilaLuta@users.noreply.github.com>
2023-04-16 13:35:51 +0200

    Update Enkriptimi.java

--- Enkriptimi.java
+++ Enkriptimi.java
@@ -1,11 +1,12 @@
+import java.util.Scanner;
 public class Enkriptimi {
-  private char[] matrica = new char[5][5];
+  private char[][] matrica = new char[5][5];
   
   public Enkriptimi(String key){
-    key = key.replaceAll("[^a-zA-Z]",""]).toUpperCase();
+    key = key.replaceAll("[^a-zA-Z]","").toUpperCase();
   }
   int row = 0, col = 0;
-  for(int i =0; i<key.length();i++){
+  for(int i = 0; i < key.length(); i++){
     char c = key.charAt(i);
     if(c == 'J'){
       c = 'I';
@@ -15,14 +16,28 @@ public class Enkriptimi {
     col++;
     if(col == 5){
     row++;
-    col=0;
+    col = 0;
     }
   }
 }
+for (char c = 'A'; c <= 'Z'; c++){
+	if(c == 'J'){
+		continue;
+	}
+	if(!contains(c)){
+	   matrica[row][col] = c;
+	   col++;
+ 	if(col == 5){
+	   row++;
+	   col=0;
+	}
+      }
+   }
+}
 private boolean contains(char c){
-  for(int row = 0; row <5; row++){
-  for (int col =0; col < 5; col++){
-  if(matrica[row][col]==c}){
+  for(int row = 0; row < 5; row++){
+  for (int col = 0; col < 5; col++){
+  if(matrica[row][col]==c){
     return true;
         }
       }
@@ -52,7 +67,7 @@ private boolean contains(char c){
         for (int i = 0; i < sb.length(); i += 2) {
             char a = sb.charAt(i);
             char b = sb.charAt(i + 1);
- int row1 = -1, col1 = -1, row2 = -1, col2 = -1;
+            int row1 = -1, col1 = -1, row2 = -1, col2 = -1;
             for (int row = 0; row < 5; row++) {
                 for (int col = 0; col < 5; col++) {
                     if (matrica[row][col] == a) {
@@ -65,15 +80,14 @@ private boolean contains(char c){
                 }
             }
             if (row1 == row2) {
-                // Same row
+                // I njejti rresht
                 ciphertext.append(matrica[row1][(col1 + 1) % 5]);
                 ciphertext.append(matrica[row2][(col2 + 1) % 5]);
             } else if (col1 == col2) {
-                // Same column
+                // I njejti kolon
                 ciphertext.append(matrica[(row1 + 1) % 5][col1]);
                 ciphertext.append(matrica[(row2 + 1) % 5][col2]);
             } else {
-                // Rectangle
                 ciphertext.append(matrica[row1][col2]);
                 ciphertext.append(matrica[row2][col1]);
             }
@@ -84,8 +98,8 @@ private boolean contains(char c){
 public static void main(String[]args){
   Scanner scanner = new Scanner(System.in);
   
-  System.out.print("Enter the key: "):
-  Sring key = scanner.nextLine();
+  System.out.print("Enter the key: ");
+  String key = scanner.nextLine();
   
   System.out.print("Enter the plaintext: ");
   String plaintext = scanne.nextLine();
@@ -96,11 +110,13 @@ public static void main(String[]args){
   System.out.println("Ciphertext: " + ciphertext);
 }
 public void displaymatrica() {
-		// TODO Auto-generated method stub
+		//Gjeneron metoden automatikisht
 		for (int row = 0; row < 5; row++) {
             for (int col = 0; col<5;col++) {
                     System.out.print(matrica[row][col] + " ");
                     }
                     System.out.println();
                 }
+	}
+}
 
